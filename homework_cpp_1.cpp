c901eee547a35e4f53f2f8122b1961af783d4b3a
Oleksandr <fedorut20@gmail.com>
2023-05-20 16:14:24 +0300

    task 2 done

--- homework_cpp_1.cpp
+++ homework_cpp_1.cpp
@@ -1,6 +1,6 @@
 /*
  homework_cpp_1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-
+ feature/hm/ex_2
 */
 
 #include <iostream>
@@ -8,10 +8,25 @@
 #include <cmath>
 #include <iomanip>
 using namespace std;
-int main()
-{
-    std::cout << "Hello World!\n";
+
+int main() {
+    SetConsoleCP(1251); SetConsoleOutputCP(1251);
+    double L; // The length of the circle
+    double R, S; // The radius and area of the circle
+    double pi = 3.14159; // The value of pi
+
+    cout << "Введіть довжину кола: ";
+    cin >> L;
+
+    R = L / (2 * pi); // Calculate the radius using the formula L = 2 * pi * R
+    S = pi * pow(R, 2); // Calculate the area using the formula S = pi * R^2
+
+    cout << "Радіус кола дорівнює: " << R << endl;
+    cout << "Площа круга дорівнює: " << S << endl;
+
+    return 0;
 }
+
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
