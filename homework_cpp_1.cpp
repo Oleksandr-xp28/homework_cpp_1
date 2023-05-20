39dd7389b3b63a02d8663dfe7fbc0fe7793e0278
Oleksandr <fedorut20@gmail.com>
2023-05-20 15:55:37 +0300

    task 1 done

--- homework_cpp_1.cpp
+++ homework_cpp_1.cpp
@@ -1,6 +1,6 @@
 /*
  homework_cpp_1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-
+ feature/hm/ex_1
 */
 
 #include <iostream>
@@ -8,10 +8,28 @@
 #include <cmath>
 #include <iomanip>
 using namespace std;
-int main()
-{
-    std::cout << "Hello World!\n";
+
+#include <iostream>
+
+int main() {
+    double R1, R2, R3;
+
+    cout << "Enter the values of R1: ";
+    cin >> R1;
+
+    cout << "Enter the values of R2: ";
+    cin >> R2;
+
+    cout << "Enter the values of R3: ";
+    cin >> R3;
+
+    double R0 = 1 / (1 / R1 + 1 / R2 + 1 / R3);
+
+    cout << "The value of resistance R0 is: " << R0 << endl;
+
+    return 0;
 }
+
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
