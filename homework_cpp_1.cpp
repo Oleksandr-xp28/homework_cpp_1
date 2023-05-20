55b3a905600514f0d139a4c7407c209ce0df0661
Oleksandr <fedorut20@gmail.com>
2023-05-20 16:28:49 +0300

    task 3 done

--- homework_cpp_1.cpp
+++ homework_cpp_1.cpp
@@ -3,16 +3,38 @@
 
 */
 
-#include <iostream>
-#include <windows.h>
-#include <cmath>
-#include <iomanip>
-using namespace std;
-int main()
-{
-    std::cout << "Hello World!\n";
+#include <iostream> // Allows program to perform input and output
+#include <windows.h> // Allows program to use system("pause")
+#include <cmath> // Allows program to use pow()
+#include <iomanip> // Allows program to use setprecision()
+using namespace std; // Program uses names from the std namespace
+
+int main() { // Function main begins program execution
+    SetConsoleCP(1251); // Set the console code page to 1251 (Cyrillic)
+    SetConsoleOutputCP(1251); // Set the console code page to 1251 (Cyrillic)
+    double v; // Initial velocity or speed
+    double t; // Time
+    double a; // Acceleration
+    double S; // Distance traveled
+
+    cout << "Введіть початкову швидкість: "; // Prompt user to enter the initial velocity
+    cin >> v; // Read the initial velocity
+
+    cout << "Введіть час: "; // Prompt user to enter the time
+    cin >> t; // Read the time
+
+    cout << "Введіть прискорення: "; // Prompt user to enter the acceleration
+    cin >> a;   // Read the acceleration
+
+    S = v * t + (a * t * t) / 2; // Calculate the distance traveled
+
+    cout << "Пройдена відстань становить: " << S << endl; // Display the distance traveled
+
+    return 0; // Indicate the program ended successfully
 }
 
+
+// Path: homework_cpp_2.cpp
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
 
